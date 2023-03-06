f8b77f0b665cffe407e4c61e79972bdcb4eec1b0
Mikhail Lavrov <lavrov.fd@gmail.com>
2023-03-06 12:53:12 +0400

    inventory FC refactoring (add usecallback & usememo hooks for optim)

--- src/features/company/inventory/Inventory.js
+++ src/features/company/inventory/Inventory.js
@@ -1,68 +1,87 @@
-import { useState } from 'react';
+import { useState, useMemo, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { deleteInventoryThunk, updateInventoryThunk } from '../companySlice';
 import c from './Inventory.module.css';
 
-export const Inventory = (props) => {
-  const { selectedPlace, statePlaces, isThereInventory } = props;
-  
+export const Inventory = ({ selectedPlace, statePlaces, isThereInventory }) => {
   const dispatch = useDispatch();
   const [editItemId, setEditItemId] = useState(null);
   const [count, setCount] = useState('');
 
-  return (
-    <ul className={c.inventory}>
-      {isThereInventory.map((item) => (
-        <li className={c.inventory__item} 
-            key={item.id}>
-          {editItemId === item.id 
-          ? (
-            <form className={c.inventory__form}
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                    dispatch(updateInventoryThunk(item.id, count));
-                    setEditItemId(null)}}>
-              <p>Название <span>{item.name}</span></p>
-              <input
-                type="number"
-                value={count}
-                onChange={(e) => setCount(e.target.value)}
-              />
-              <button type='submit'>Сохранить</button>
-            </form>
-          ) : (
-            <>
-              <p>Название <span>{item.name}</span></p>
-              <div>
-                Количество{" "}
-                {statePlaces?.filter((place) => place.id === selectedPlace)[0]?.parts 
-                ? (
-                  <span>{item.count}</span>
-                ) : (
-                  <>
-                    <span id="itemCount">{item.count}</span>
-                    <p className={c.inventory__buttons}>
-                      <button
-                        className={c.inventory__editButton}
-                        type="button"
-                        onClick={() => {
-                          setEditItemId(item.id)
-                          setCount(item.count)
-                        }}
-                      > &#9998;</button>
-                      <button
-                        className={c.inventory__delButton}
-                        type="button"
-                        onClick={() => dispatch(deleteInventoryThunk(item.id))}
-                      >&#10006;</button>
-                    </p>
-                  </>
-                )}
-              </div>
-            </>
-          )}
-        </li>
-      ))}
-    </ul>
+  const handleEditItem = useCallback(
+    (item) => {
+      setEditItemId(item.id);
+      setCount(item.count);
+    }, []
   );
+  
+  const handleUpdateInventory = useCallback(
+    (item) => {
+      dispatch(updateInventoryThunk(item.id, count));
+      setEditItemId(null);
+    }, [count, dispatch]
+  );
+
+  const inventoryItems = useMemo(
+    () =>
+      isThereInventory.map((item) => {
+        const selectedPlaceParts = statePlaces?.filter((place) => place.id === selectedPlace)[0]?.parts || false;
+
+        return (
+          <li className={c.inventory__item} key={item.id}>
+            {editItemId === item.id ? (
+              <form
+                className={c.inventory__form}
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  handleUpdateInventory(item);
+                }}
+              >
+                <p>
+                  Название <span>{item.name}</span>
+                </p>
+                <input type="number" value={count} onChange={(e) => setCount(e.target.value)} />
+                <button type="submit">Сохранить</button>
+              </form>
+            ) : (
+              <>
+                <p>
+                  Название <span>{item.name}</span>
+                </p>
+                <div>
+                  Количество{" "}
+                  {selectedPlaceParts ? (
+                    <span>{item.count}</span>
+                  ) : (
+                    <>
+                      <span id="itemCount">{item.count}</span>
+                      <p className={c.inventory__buttons}>
+                        <button
+                          className={c.inventory__editButton}
+                          type="button"
+                          onClick={() => handleEditItem(item)}
+                        >
+                          {" "}
+                          &#9998;
+                        </button>
+                        <button
+                          className={c.inventory__delButton}
+                          type="button"
+                          onClick={() => dispatch(deleteInventoryThunk(item.id))}
+                        >
+                          &#10006;
+                        </button>
+                      </p>
+                    </>
+                  )}
+                </div>
+              </>
+            )}
+          </li>
+        );
+      }),
+    [count, dispatch, editItemId, handleEditItem, handleUpdateInventory, isThereInventory, selectedPlace, statePlaces]
+  );
+
+  return <ul className={c.inventory}>{inventoryItems}</ul>;
 };
